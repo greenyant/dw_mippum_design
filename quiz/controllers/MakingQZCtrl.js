c74d8e52fb61818aa7abc3659f9367162eb5e572
Kwak, Jaehung <wogud86@gmail.com>
2016-03-29 11:20:30 +0900

    design is changed and ct file add, download, del is working.

--- quiz/controllers/MakingQZCtrl.js
+++ quiz/controllers/MakingQZCtrl.js
@@ -1,52 +1,59 @@
 angular.module("makingQZ")
 .controller("MakingQZCtrl", function ($scope){
     $scope.const_str = const_str;
-    
+    $scope.qz = {
+        "name":"",
+        "itmes": []
+    };
     
     $scope.open_mpqz_file = function(event){
         var file = event.target.files[0];
         var reader = new FileReader();
+        
+        
         reader.onload = function(progressEvent){
             // Entire file
             $scope.qz = angular.fromJson(this.result);
-            
-            
-            
             $scope.$apply();
         };
         reader.readAsText(file);
-    }
+    };
     
-     $scope.open_mpct_file = function(event){
+    //start of area for ct
+    $scope.open_mpct_file = function(event){
         var file = event.target.files[0];
         var reader = new FileReader();
         reader.onload = function(progressEvent){
-            // Entire file
-            
-            //$scope.qz.ct = angular.fromJson(this.result);
-            /*
-            $scope.select_order = [];
-            $scope.selected_contents = [];
-            
-            for(var i=0; i<$scope.ct.level_label.length; i++){
-                if(i==0){
-                    var selected_one = $scope.ct.contents;
-                    $scope.select_order.push(0);
-                    $scope.selected_contents.push(selected_one);
-                } else {
-                    var selected_one = selected_one[$scope.select_order[i-1]].sub_contents;
-                    $scope.select_order.push(0);
-                    $scope.selected_contents.push(selected_one);
-                }
-                for(var j=0; j<$scope.mark_types.length; j++){
-                    if($scope.mark_types[j].keyword == $scope.ct.level_label[i].mark_type){
-                        $scope.ct.level_label[i].mark_example = $scope.mark_types[j].example;
-                    }
-                }
+            if($scope.qz.ct == undefined){
+                $scope.qz.ct =[];
             }
-            */
+            new_ct = angular.fromJson(this.result);
+            new_ct.order = $scope.qz.ct.length;
+            $scope.qz.ct.push(new_ct);
+            
             $scope.$apply();
         };
         reader.readAsText(file);
-    }
+    };
+    
+    $scope.down_mpct_file = function(order){
+    	//copy and delete order before download.
+        var down_ct = angular.fromJson(angular.toJson($scope.qz.ct[order]));
+        delete down_ct.order;
+        download(angular.toJson(down_ct, true), 
+                 down_ct.name+".mpct", "text/plain");
+    };
+    $scope.del_ct = function(order){
+        $scope.qz.ct.splice(order, 1);
+        for(var i=order; i<$scope.qz.ct.length; i++){
+        	$scope.qz.ct[i].order = i;
+        }
+    };
+    //end of area for ct
+     
+    $scope.down_mpqz_file = function(){
+        //download($scope.down_dat = angular.toJson($scope.qz, true), 
+        //        $scope.ct.name+".mpct", "text/plain");
+    };
+    
 });
