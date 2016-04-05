2227ed960536fd4ac52de77e88a705d3f3364cc3
Kwak, Jaehung <wogud86@gmail.com>
2016-04-05 20:03:36 +0900

    choiceMulti is showing in the solve_with_son.html.

--- quiz/controllers/solvingCtrl.js
+++ quiz/controllers/solvingCtrl.js
@@ -26,6 +26,8 @@ angular.module("solve_quiz")
 			return;
 		}
 		$scope.quiz = quiz.quizData;
+		
+		
 		if($scope.quiz.usr_index == undefined) {
 			$scope.quiz.usr_index = 0;
 		}
@@ -50,6 +52,9 @@ angular.module("solve_quiz")
 			$scope.ans_btn_show_str = $scope.const_str.btn_show_str[0];
 			$scope.sol_show_flag = false;
 			$scope.sol_btn_show_str = $scope.const_str.btn_show_str[0];
+			
+			if(typeof(MathJax)!== 'undefined' && $scope.quiz.using_math_flag) setTimeout(function(){ MathJax.Hub.Typeset();}, 10);
+			//if(typeof(MathJax)!== 'undefined') console.log(MathJax);
 		};
 		reset();
 		
@@ -66,12 +71,41 @@ angular.module("solve_quiz")
 		});*/
 		
 		$scope.selectResponse = function(choice_order) {
-			$scope.response = choice_order;
-			$scope.quiz.items[$scope.quiz.usr_index].response = choice_order;
+			var item = $scope.quiz.items[$scope.quiz.usr_index];
+			if(item.answerType=='choiceMulti'){
+				if(item.choices[choice_order].response == undefined)
+					item.choices[choice_order].response = false;
+				item.choices[choice_order].response = !item.choices[choice_order].response; 
+				//console.log(item.choices[choice_order]);
+				item.response = [];
+				for(var i=0; i<item.choices.length;i++){
+					if(item.choices[i].response) item.response.push(i);
+				}
+				if(JSON.stringify(item.answer) == JSON.stringify(item.response))
+					item.response = item.answer;
+			} else {
+				$scope.response = choice_order;
+				item.response = choice_order;
+			}
 		};
 		$scope.clickSubmit = function(){
+			var item = $scope.quiz.items[$scope.quiz.usr_index];
 			//$scope.response_flag = true;
-			$scope.quiz.items[$scope.quiz.usr_index].complete_flag = true;
+			item.complete_flag = true;
+			
+			if(item.answerType=='choiceMulti'){
+				item.answerText = "";
+				for(var i=0; i<item.answer.length;i++){
+					item.answerText += item.choices[item.answer[i]].value;
+					if(i != item.answer.length -1) {
+						if(item.numOfChoicesEachRow == 1) item.answerText += '<br />';
+						else item.answerText += ', ';
+					}
+				}
+			} else if(item.answerType=='choiceOne'){
+				item.answerText = item.choices[item.answer].value;
+			}
+			if(typeof(MathJax)!== 'undefined' && $scope.quiz.using_math_flag) setTimeout(function(){ MathJax.Hub.Typeset();}, 10);
 		};
 		$scope.clickShowAns = function(){
 			$scope.ans_show_flag = !$scope.ans_show_flag;
@@ -97,4 +131,7 @@ angular.module("solve_quiz")
 			$scope.quiz.usr_index -= 1;
 			reset();
 		};
+		//console.log($scope.quiz);
+		//if($scope.quiz.using_math_flag) setTimeout(function(){ MathJax.Hub.Typeset();}, 10);
+		
 	});
