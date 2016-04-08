712d5f63710ad20d247d7a1c352add206b7cc204
Kwak, Jaehung <wogud86@gmail.com>
2016-04-08 10:56:06 +0900

    mathjax is dynamic loaded.

--- quiz/controllers/solvingCtrl.js
+++ quiz/controllers/solvingCtrl.js
@@ -26,7 +26,41 @@ angular.module("solve_quiz")
 			return;
 		}
 		$scope.quiz = quiz.quizData;
-		
+
+		if($scope.quiz.using_math_flag){
+			var script = document.createElement('script');
+			var head = document.getElementsByTagName("head")[0], script;
+			script = document.createElement("script");
+			script.type = "text/x-mathjax-config";
+			script[(window.opera ? "innerHTML" : "text")] =
+				'MathJax.Hub.Config({'+
+				'	jax: ["input/TeX", "output/SVG"],'+
+				'	tex2jax: {'+
+				"		inlineMath: [['$','$']],"+
+				"		displayMath: [ ['$$','$$']],"+
+				'		processEscapes: true,'+
+				'		processEnvironments: true,'+
+				'		ignoreClass: "no-mathjax",'+
+				'		processClass: "mathjax",'+
+				'		preview: "TeX"'+
+				'	},'+
+				'	"HTML-CSS": { '+
+				'		availableFonts: ["STIX", "TeX"],'+
+				'		webFont: null '+
+				'	},'+
+				'	"SVG": {'+
+				'		availableFonts: ["TeX"],'+
+				'		blacker: 0,'+
+				'		scale: 90'+
+				'	},'+
+				'	showMathMenu: false'+
+				'});';
+			head.appendChild(script);
+			script = document.createElement("script");
+			script.type = "text/javascript";
+			script.src = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG-full';
+			head.appendChild(script);
+		}		
 		
 		if($scope.quiz.usr_index == undefined) {
 			$scope.quiz.usr_index = 0;
