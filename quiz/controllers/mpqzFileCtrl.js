4f12a07cc931137763412dea38750ec778dd1657
Kwak, Jaehung <wogud86@gmail.com>
2016-04-08 14:29:52 +0900

    mpqz file using with uri, it can be worked directly for solve_with_mpqz_file.

--- quiz/controllers/mpqzFileCtrl.js
+++ quiz/controllers/mpqzFileCtrl.js
@@ -1,11 +1,34 @@
 // JavaScript Document
+window.params = function(){
+    var params = {};
+    if(window.location.href.indexOf('?') == -1){
+        return params;
+    }
+    var param_array = window.location.href.split('?')[1].split('&');
+    for(var i in param_array){
+        x = param_array[i].split('=');
+        params[x[0]] = x[1];
+    }
+    return params;
+}();
+
 angular.module("solve_quiz")
 	.controller("mpqzFileNabvarCtrl", function Ctrl($scope, quiz){
 		$scope.name = const_str_mpqzfile.input_text;
 	});
-	
 angular.module("solve_quiz")
-	.controller("mpqzFileCtrl", function Ctrl($scope, quiz){
+	.controller("mpqzFileCtrl", function Ctrl($scope, $http, quiz){
+		//ex : uri=dat_files/sample_M3.mpqz
+		if(window.params.uri != undefined){
+			$http.get(window.params.uri).success(function(data){
+				
+				//console.log(data);
+				$scope.quiz  = data;
+				$scope.err_msg_apply_quiz_json = "";
+				quiz.quizData = $scope.quiz;
+				location.replace("#/solving");
+			});
+		}
 		
 		$scope.clickApplyQuizJsonBtn = function(){
 			try { 
